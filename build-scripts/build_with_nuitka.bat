de04c6feb9f62c5fd8adc4eadaed72de8bc4a93b
HowieHz <howie_hzgo@outlook.com>
2024-12-01 13:12:43 +0800

    chore: update build script

--- build-scripts/build_with_nuitka.bat
+++ build-scripts/build_with_nuitka.bat
@@ -1,2 +1,9 @@
-nuitka .\src\main.py --standalone --onefile --include-data-files=.venv\Lib\site-packages\whois21\vcard-map.json=whois21\vcard-map.json --output-filename=domain-checker --output-dir=bin
-nuitka .\extra-scripts\travellings-cn-api\main.py --standalone --onefile --output-filename=travellings-cn-api --output-dir=bin
+@echo off
+
+echo build domain-checker 
+
+start nuitka .\src\main.py --standalone --onefile --include-data-files=.venv\Lib\site-packages\whois21\vcard-map.json=whois21\vcard-map.json --output-filename=domain-checker --output-dir=bin\domain-checker
+
+echo build travellings-cn-api
+
+start nuitka .\extra-scripts\travellings-cn-api\main.py --standalone --onefile --output-filename=travellings-cn-api --output-dir=bin\travellings-cn-api
--- build-scripts/build_with_nuitka.sh
+++ build-scripts/build_with_nuitka.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-nuitka ./src/main.py --standalone --onefile --include-data-files=.venv/Lib/site-packages/whois21/vcard-map.json=whois21/vcard-map.json --output-filename=domain-checker --output-dir=bin
-nuitka ./extra-scripts/travellings-cn-api/main.py --standalone --onefile --output-filename=travellings-cn-api --output-dir=bin
+echo "build domain-checker"
+nuitka ./src/main.py --standalone --onefile --include-data-files=.venv/Lib/site-packages/whois21/vcard-map.json=whois21/vcard-map.json --output-filename=domain-checker --output-dir=bin/domain-checker
+
+echo "build travellings-cn-api"
+nuitka ./extra-scripts/travellings-cn-api/main.py --standalone --onefile --output-filename=travellings-cn-api --output-dir=bin/travellings-cn-api
