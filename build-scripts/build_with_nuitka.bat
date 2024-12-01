6c7af512f812df46e86f0c33450d0ef51437c280
HowieHz <howie_hzgo@outlook.com>
2024-12-01 12:59:56 +0800

    chore: update build script

--- build-scripts/build_with_nuitka.bat
+++ build-scripts/build_with_nuitka.bat
@@ -1,1 +1,2 @@
 nuitka .\src\main.py --standalone --onefile --include-data-files=.venv\Lib\site-packages\whois21\vcard-map.json=whois21\vcard-map.json --output-filename=domain-checker --output-dir=bin
+nuitka .\extra-scripts\travellings-cn-api\main.py --standalone --onefile --output-filename=travellings-cn-api --output-dir=bin
